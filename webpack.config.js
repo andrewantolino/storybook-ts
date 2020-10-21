e6f139ff641fe5524b6350fbb1e1a6e22513eb42
Andrew Antolino <andrew@smithbrothersmedia.com.au>
2020-10-21 22:26:40 +1100

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(\__dirname, "src", "components", "index.html"),
+      template: path.resolve(__dirname, "src", "components", "index.html"),
     }),
     new MiniCssExtractPlugin({
       filename: "./src/yourfile.css",
